fdd03980466348397339e20ce13ea7a09ec68dc3
cogg <adam@cogg.rocks>
2021-11-06 19:53:03 -0700

    add cli

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::{collections::HashMap, path::Path, rc::Rc};
 
+use clap::{Parser as CmdParser};
+
 mod common;
 
 mod lexer;
@@ -10,6 +12,26 @@ mod disjoint_set;
 mod symbols;
 mod assets;
 
+#[derive(CmdParser)]
+#[clap(version = "1.0.0", author = "cogg <adam@cogg.rocks>")]
+struct CmdOptions {
+    /// The source file to compile.
+    filename: String,
+    #[clap(default_value = "main")]
+    /// The name of the top-level module to generate a blueprint for.
+    mod_name: String,
+
+    #[clap(long)]
+    /// Disable all optimizations.
+    no_opt: bool,
+    #[clap(long)]
+    /// Disable constant folding.
+    no_fold: bool,
+    #[clap(long)]
+    /// Disable pruning unused combinators.
+    no_prune: bool
+}
+
 #[derive(Debug)]
 pub struct CompileSettings {
     fold_constants: bool,
@@ -19,16 +41,16 @@ pub struct CompileSettings {
 
 fn main() {
 
-    let main_mod_name = std::env::args().nth(1).unwrap_or_else(|| "main".into());
+    let options = CmdOptions::parse();
 
     // TODO allow users to override or add additional symbols
     let symbols_json = assets::get_asset_string("symbols.json").expect("failed to load symbol defintions");
     symbols::load_symbols(&symbols_json);
 
     let settings = Rc::new(CompileSettings{
-        fold_constants: true,
-        prune: true,
-        main_mod_name
+        fold_constants: !(options.no_fold || options.no_opt),
+        prune: !(options.no_prune || options.no_opt),
+        main_mod_name: options.mod_name
     });
 
     let mut modules = HashMap::new();
@@ -38,7 +60,7 @@ fn main() {
         ir::build_ir(prelude_parsed, settings.clone(), &mut modules);
     };
 
-    let source = std::fs::read_to_string("projects/test.cdl").expect("failed to read file");
+    let source = std::fs::read_to_string(options.filename).expect("failed to read file");
     let parse_results = crate::parser::parse(&source);
     ir::build_ir(parse_results, settings.clone(), &mut modules);
 
